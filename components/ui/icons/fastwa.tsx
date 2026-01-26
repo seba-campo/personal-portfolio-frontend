77bb4d713e9897c6f9157e65f5e472a63d1ede42
Sebastian Campo <campo.sebastian44@gmail.com>
2026-01-25 21:50:39 -0300

    fixed icons

--- components/ui/icons/fastwa.tsx
+++ components/ui/icons/fastwa.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const FastwaIcon = () => {
     return (
         <div>
-            <Image src="/fastwaIcon.png" alt="fastwa" width={28} height={28} style={{ borderRadius: "4px" }} />
+            <Image src="/fastwaIcon.jpg" alt="f" width={28} height={28} style={{ borderRadius: "4px" }} />
         </div>
     )
 }
